ba061d27490edd9cb50aaebdb44cc9df68b079fa
Alex Clarke <clarkexcore@gmail.com>
2020-03-29 23:20:47 -0400

    YouTube videos added in no problem

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,4 +1,7 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
+
+//Packages
+import YouTube from 'react-youtube';
 
 //Components
 import HomeHeader from '../layouts/HomeHeader';
@@ -9,6 +12,45 @@ import ShopData from '../../data/shop.json';
 
 const Homepage = () => {
 
+    const [videos, setVideos] = useState(null);
+    const [videoLoading, setVideoLoading] = useState(true);
+
+    useEffect(() => {
+        getVideos();
+    }, []);
+
+
+    //Get Videos from YouTube
+    const getVideos = () => {
+        const apiKey = process.env.REACT_APP_GOOGLE_API;
+        const channelID = 'UCUFdaYrXodRczmBWQKighMQ';
+        const maxResults = 5;
+        const url = "https://www.googleapis.com/youtube/v3/search?key=" +
+        apiKey +
+        "&channelId=" +
+        channelID +
+        "&part=snippet,id&order=date&maxResults=" +
+        maxResults;
+
+
+        fetch(url)
+            .then(res => {
+                if(res.status >= 400){
+                    throw new Error("Bad Respsonse From Server");
+                }
+                return res.json();
+            })
+            .then(data => {
+                console.log(data);
+                setVideos(data.items);
+                setVideoLoading(false);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+
+    //JSX Elements
     const shopData = ShopData.map((item, index) => {
         if(index < 1){
             return(
@@ -18,7 +60,27 @@ const Homepage = () => {
                 </Fragment>
             )
         }
-    })
+    });
+
+    const videoPlayer = () => (
+        <Fragment>
+            {!videoLoading ? 
+                videos.map(video => (
+                    <div className="videoContainer">
+                        <YouTube
+                            videoId={video.id.videoId}
+                            className="yt-embed"
+                            opts={{
+                                playerVars: {
+                                autoplay: 0
+                                }
+                            }}
+                        />
+                    </div>
+                ))
+            : null}
+        </Fragment>
+    );
 
     return(
         <Fragment>
@@ -26,6 +88,7 @@ const Homepage = () => {
             <div className="gradient-bar"></div>
             <Navigation />
             {shopData}
+            {videoPlayer()}
         </Fragment>
     );
 }
